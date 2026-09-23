d5346eb3eea96445ab02c56a2b41730d9bd7925f
vchandwani <varunchandwani@gmail.com>
2026-09-24 09:46:45 +1000

    Base App for GraphQL React App
    Base App for GraphQL React App

--- server/server.js
+++ server/server.js
@@ -1,38 +1,36 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
 
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = "mongodb+srv://varunchandwani_db_user:jMgAeRzXclhSKI2z@cluster0.vyxlehy.mongodb.net/?appName=Cluster0";
 if (!MONGO_URI) {
-  throw new Error('You must provide a Mongo Atlas URI');
+  throw new Error("You must provide a Mongo Atlas URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('open', () => console.log('Connected to Mongo Atlas instance.'))
-  .on('error', (error) =>
-    console.log('Error connecting to Mongo Atlas:', error)
-  );
+  .once("open", () => console.log("Connected to Mongo Atlas instance."))
+  .on("error", (error) => console.log("Error connecting to Mongo Atlas:", error));
 
 app.use(bodyParser.json());
 app.use(
-  '/graphql',
+  "/graphql",
   expressGraphQL({
     schema,
-    graphiql: true
-  })
+    graphiql: true,
+  }),
 );
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
