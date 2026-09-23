91489281d3024f438b6c37de4166b6f6283a15b3
vchandwani <varunchandwani@gmail.com>
2026-09-24 09:55:47 +1000

    Initial setup for Apollo Setup
    Initial setup for Apollo Setup

--- client/index.js
+++ client/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+});
 
 const Root = () => {
-  return <div>Lyrical</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
