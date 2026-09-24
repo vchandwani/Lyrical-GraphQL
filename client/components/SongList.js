983ad7b35b982a5dc8dbdd3e587cac765594c167
vchandwani <varunchandwani@gmail.com>
2026-09-24 11:00:46 +1000

    Song List shown
    Song List shown

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Spinner from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TableContainer from "@mui/material/TableContainer";
@@ -24,7 +25,12 @@ const SongList = () => {
   // Execute the query inside the component using the hook
   const { loading, error, data } = useQuery(FETCH_SONGS);
 
-  if (loading) return <Typography sx={{ p: 2 }}>Loading songs...</Typography>;
+  if (loading)
+    return (
+      <Spinner size="30px" aria-label="Loading…">
+        Loading songs...
+      </Spinner>
+    );
   if (error) return <Typography sx={{ p: 2, color: "error.main" }}>Error: {error.message}</Typography>;
 
   return (
